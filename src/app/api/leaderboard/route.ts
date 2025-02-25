98e6b23ce55ae575c30bee8e98881040816a0d9c
jrKitt <kittichdev@gmail.com>
2025-02-26 02:47:05 +0700

    feat: moodengpop by poplab games

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -4,40 +4,52 @@ import path from "path";
 
 const filePath = path.join(process.cwd(), "data", "leaderboard.json");
 
-const readData = () => {
+const readData = (): { name: string; score: number }[] => {
   try {
     const data = fs.readFileSync(filePath, "utf8");
-    return JSON.parse(data);
-  } catch (error) {
+    return JSON.parse(data) as { name: string; score: number }[];
+  } catch {
     return [];
   }
 };
-const writeData = (data: any) => {
+
+const writeData = (data: { name: string; score: number }[]) => {
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf8");
 };
+
 export async function GET() {
   const leaderboard = readData();
   return NextResponse.json(leaderboard);
 }
+
 export async function POST(req: Request) {
+  try {
     const { name, score } = await req.json();
     if (!name || score === undefined) {
-      return NextResponse.json({ success: false, error: "Missing name or score" }, { status: 400 });
+      return NextResponse.json(
+        { success: false, error: "Missing name or score" },
+        { status: 400 }
+      );
     }
-  
+
     let leaderboard = readData();
     const existingUser = leaderboard.find((player) => player.name === name);
-  
+
     if (existingUser) {
-      existingUser.score += 1; 
+      existingUser.score += 1;
     } else {
       leaderboard.push({ name, score: 1 });
     }
-  
+
     leaderboard.sort((a, b) => b.score - a.score);
-    leaderboard = leaderboard.slice(0, 10); 
-  
+    leaderboard = leaderboard.slice(0, 10);
+
     writeData(leaderboard);
     return NextResponse.json({ success: true, leaderboard });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: error},
+      { status: 500 }
+    );
   }
-  
+}
